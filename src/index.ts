3ddf8663f9c021df3b96f4f1d9dacc1b69ed4946
Eugene Cheung <ey.cheung@outlook.com>
2016-08-20 10:44:33 -0400

    Remove provider from module

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import InlineSVGDirective from './inline-svg.directive';
-import SVGCache from './svg-cache.service';
 
 export { InlineSVGDirective };
 
 @NgModule({
   declarations: [InlineSVGDirective],
   imports: [HttpModule],
-  exports: [InlineSVGDirective],
-  providers: [SVGCache]
+  exports: [InlineSVGDirective]
 })
 export class InlineSVGModule {}
