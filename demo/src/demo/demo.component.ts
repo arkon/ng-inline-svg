21a11e3da60b572a0dca1675c18dce31732ad208
Eugene Cheung <ey.cheung@outlook.com>
2017-04-04 13:31:42 -0400

    Updates

--- demo/src/demo/demo.component.ts
+++ demo/src/demo/demo.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'demo',
   template: `
     <div class="demo-svg1" aria-label="My icon 1" [inlineSVG]="'img/image.svg'"></div>
-    <div class="demo-svg2" aria-label="My icon 2" [inlineSVG]="'img/image.svg'"></div>
+    <div class="demo-svg2" aria-label="My icon 2" [inlineSVG]="'img/image.svg'" [replaceContents]="true">Content</div>
     <div *ngIf="_showOther" class="demo-svg3" aria-label="My delayed icon" [inlineSVG]="'img/image_with_fill.svg'" [removeSVGAttributes]="['fill']"></div>
     <div [inlineSVG]="'#fish'"></div>
     <div [inlineSVG]="'img/nope.svg'" [fallbackImgUrl]="'https://nodei.co/npm/ng-inline-svg.png?compact=true'"></div>
