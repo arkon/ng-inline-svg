f2716545a90cbf4171ce811582c7109624e08079
espenandreassen <espen.andreassen@marcello.no>
2018-05-18 16:25:16 +0200

    Fix indentation

--- src/inline-svg.directive.ts
+++ src/inline-svg.directive.ts
@@ -1,19 +1,19 @@
 import {
-    ComponentFactoryResolver,
-    ComponentRef,
-    Directive,
-    ElementRef,
-    EventEmitter,
-    Inject,
-    Input,
-    OnChanges,
-    OnDestroy,
-    OnInit,
-    Output,
-    PLATFORM_ID,
-    Renderer2,
-    SimpleChanges,
-    ViewContainerRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Directive,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  PLATFORM_ID,
+  Renderer2,
+  SimpleChanges,
+  ViewContainerRef,
 } from '@angular/core';
 import {isPlatformBrowser, isPlatformServer} from '@angular/common';
 import {Subscription} from 'rxjs';
@@ -23,236 +23,236 @@ import {SVGCacheService} from './svg-cache.service';
 import {InlineSVGService} from './inline-svg.service';
 
 @Directive({
-    selector: '[inlineSVG]',
-    providers: [SVGCacheService]
+  selector: '[inlineSVG]',
+  providers: [SVGCacheService]
 })
 export class InlineSVGDirective implements OnInit, OnChanges, OnDestroy {
-    @Input() inlineSVG: string;
-    @Input() replaceContents: boolean = true;
-    @Input() prepend: boolean = false;
-    @Input() injectComponent: boolean = false;
-    @Input() cacheSVG: boolean = true;
-    @Input() removeSVGAttributes: Array<string>;
-    @Input() forceEvalStyles: boolean = false;
-    @Input() evalScripts: 'always' | 'once' | 'never' = 'always';
-    @Input() fallbackImgUrl: string;
-    @Input() onSVGLoaded: (svg: SVGElement, parent: Element | null) => SVGElement;
-    @Input() isSVGString: boolean = false;
+  @Input() inlineSVG: string;
+  @Input() replaceContents: boolean = true;
+  @Input() prepend: boolean = false;
+  @Input() injectComponent: boolean = false;
+  @Input() cacheSVG: boolean = true;
+  @Input() removeSVGAttributes: Array<string>;
+  @Input() forceEvalStyles: boolean = false;
+  @Input() evalScripts: 'always' | 'once' | 'never' = 'always';
+  @Input() fallbackImgUrl: string;
+  @Input() onSVGLoaded: (svg: SVGElement, parent: Element | null) => SVGElement;
+  @Input() isSVGString: boolean = false;
 
-    @Output() onSVGInserted: EventEmitter<SVGElement> = new EventEmitter<SVGElement>();
-    @Output() onSVGFailed: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onSVGInserted: EventEmitter<SVGElement> = new EventEmitter<SVGElement>();
+  @Output() onSVGFailed: EventEmitter<any> = new EventEmitter<any>();
 
-    /** @internal */
-    _prevSVG: SVGElement;
+  /** @internal */
+  _prevSVG: SVGElement;
 
-    private _supportsSVG: boolean;
+  private _supportsSVG: boolean;
 
-    private _prevUrl: string;
+  private _prevUrl: string;
 
-    private _ranScripts: { [url: string]: boolean } = {};
+  private _ranScripts: { [url: string]: boolean } = {};
 
-    private _subscription: Subscription;
+  private _subscription: Subscription;
 
-    private _svgComp: ComponentRef<InlineSVGComponent>;
+  private _svgComp: ComponentRef<InlineSVGComponent>;
 
-    constructor(
-        private _el: ElementRef,
-        private _viewContainerRef: ViewContainerRef,
-        private _resolver: ComponentFactoryResolver,
-        private _svgCache: SVGCacheService,
-        private _renderer: Renderer2,
-        private _inlineSVGService: InlineSVGService,
-        @Inject(PLATFORM_ID) private platformId: Object) {
-        this._supportsSVG = this._checkSVGSupport();
+  constructor(
+      private _el: ElementRef,
+      private _viewContainerRef: ViewContainerRef,
+      private _resolver: ComponentFactoryResolver,
+      private _svgCache: SVGCacheService,
+      private _renderer: Renderer2,
+      private _inlineSVGService: InlineSVGService,
+      @Inject(PLATFORM_ID) private platformId: Object) {
+    this._supportsSVG = this._checkSVGSupport();
 
-        // Check if the browser supports embed SVGs
-        if (!isPlatformServer(this.platformId) && !this._supportsSVG) {
-            this._fail('Embed SVG not supported by browser');
-        }
+    // Check if the browser supports embed SVGs
+    if (!isPlatformServer(this.platformId) && !this._supportsSVG) {
+      this._fail('Embed SVG not supported by browser');
     }
+  }
 
-    ngOnInit(): void {
-        if (!isPlatformBrowser(this.platformId) && !isPlatformServer(this.platformId)) { return; }
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId) && !isPlatformServer(this.platformId)) { return; }
 
-        this._insertSVG();
+    this._insertSVG();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!isPlatformBrowser(this.platformId) && !isPlatformServer(this.platformId)) { return; }
+
+    if (changes['inlineSVG']) {
+      this._insertSVG();
     }
+  }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        if (!isPlatformBrowser(this.platformId) && !isPlatformServer(this.platformId)) { return; }
-
-        if (changes['inlineSVG']) {
-            this._insertSVG();
-        }
+  ngOnDestroy(): void {
+    if (this._subscription) {
+      this._subscription.unsubscribe();
     }
+  }
 
-    ngOnDestroy(): void {
-        if (this._subscription) {
-            this._subscription.unsubscribe();
-        }
-    }
-
-    private _insertSVG(): void {
+  private _insertSVG(): void {
     if (!isPlatformServer(this.platformId) && !this._supportsSVG) { return; }
 
-        // Check if a URL/string was actually passed into the directive
-        if (!this.inlineSVG && !this.isSVGString) {
-            this._fail('No URL passed to [inlineSVG]');
-            return;
-        } else if (!this.inlineSVG && this.isSVGString) {
-            this._fail('No SVG string passed to [inlineSVG]');
-            return;
-        }
-
-        if (this.isSVGString) {
-            this.insertSvgElement(this._svgElementFromString(this.inlineSVG));
-        } else {
-            this.insertSvgElementFromUrl();
-        }
+    // Check if a URL/string was actually passed into the directive
+    if (!this.inlineSVG && !this.isSVGString) {
+      this._fail('No URL passed to [inlineSVG]');
+      return;
+    } else if (!this.inlineSVG && this.isSVGString) {
+      this._fail('No SVG string passed to [inlineSVG]');
+      return;
     }
 
-    private insertSvgElementFromUrl() {
-        // Support for symbol IDs
-        if (this.inlineSVG.charAt(0) === '#' || this.inlineSVG.indexOf('.svg#') > -1) {
-            const elSvg = this._renderer.createElement('svg', 'svg');
-            const elSvgUse = this._renderer.createElement('use', 'svg');
-            const absUrl = this._svgCache.getAbsoluteUrl(this.inlineSVG);
-            this._renderer.setAttribute(elSvgUse, 'href', absUrl, 'xlink');
-            this._renderer.appendChild(elSvg, elSvgUse);
+    if (this.isSVGString) {
+      this.insertSvgElement(this._svgElementFromString(this.inlineSVG));
+    } else {
+      this.insertSvgElementFromUrl();
+    }
+  }
 
-            this._insertEl(elSvg);
+  private insertSvgElementFromUrl() {
+    // Support for symbol IDs
+    if (this.inlineSVG.charAt(0) === '#' || this.inlineSVG.indexOf('.svg#') > -1) {
+      const elSvg = this._renderer.createElement('svg', 'svg');
+      const elSvgUse = this._renderer.createElement('use', 'svg');
+      const absUrl = this._svgCache.getAbsoluteUrl(this.inlineSVG);
+      this._renderer.setAttribute(elSvgUse, 'href', absUrl, 'xlink');
+      this._renderer.appendChild(elSvg, elSvgUse);
 
-            this.onSVGInserted.emit(elSvg);
-            return;
-        }
+      this._insertEl(elSvg);
 
-        if (this.inlineSVG !== this._prevUrl) {
-            this._prevUrl = this.inlineSVG;
-
-            // Fetch SVG via cache mechanism
-            this._subscription = this._svgCache.getSVG(this.inlineSVG, this.cacheSVG)
-                .subscribe(
-                    (svgElement: SVGElement) => this.insertSvgElement(svgElement),
-                    (err: any) => this._fail(err)
-                );
-        }
+      this.onSVGInserted.emit(elSvg);
+      return;
     }
 
-    private insertSvgElement(svg: SVGElement) {
-        if (!svg) { return; }
+    if (this.inlineSVG !== this._prevUrl) {
+      this._prevUrl = this.inlineSVG;
 
-        // Insert SVG
-        if (this.removeSVGAttributes) {
-            this._removeAttributes(svg, this.removeSVGAttributes);
-        }
+      // Fetch SVG via cache mechanism
+      this._subscription = this._svgCache.getSVG(this.inlineSVG, this.cacheSVG)
+          .subscribe(
+              (svgElement: SVGElement) => this.insertSvgElement(svgElement),
+              (err: any) => this._fail(err)
+          );
+    }
+  }
 
-        if (this.onSVGLoaded) {
-            svg = this.onSVGLoaded(svg, this._el.nativeElement);
-        }
+  private insertSvgElement(svg: SVGElement) {
+    if (!svg) { return; }
 
-        this._insertEl(svg);
-
-        // Script evaluation
-        this._evalScripts(svg, this.inlineSVG);
-
-        // Force evaluation of <style> tags since IE doesn't do it.
-        // See https://github.com/arkon/ng-inline-svg/issues/17
-        if (this.forceEvalStyles) {
-            const styleTags = svg.querySelectorAll('style');
-            Array.from(styleTags).forEach(tag => tag.textContent += '');
-        }
-
-        this.onSVGInserted.emit(svg);
+    // Insert SVG
+    if (this.removeSVGAttributes) {
+      this._removeAttributes(svg, this.removeSVGAttributes);
     }
 
-    private _svgElementFromString(str: string): SVGElement | never {
-        const div = this._renderer.createElement('DIV');
-        div.innerHTML = str;
-
-        return div.querySelector('svg') as SVGElement;
+    if (this.onSVGLoaded) {
+      svg = this.onSVGLoaded(svg, this._el.nativeElement);
     }
 
-    private _insertEl(el: Element): void {
-        if (this.injectComponent) {
-            if (!this._svgComp) {
-                const factory = this._resolver.resolveComponentFactory(InlineSVGComponent);
-                this._svgComp = this._viewContainerRef.createComponent(factory);
-            }
+    this._insertEl(svg);
 
-            this._svgComp.instance.context = this;
-            this._svgComp.instance.replaceContents = this.replaceContents;
-            this._svgComp.instance.prepend = this.prepend;
-            this._svgComp.instance.content = el;
+    // Script evaluation
+    this._evalScripts(svg, this.inlineSVG);
 
-            // Force element to be inside the directive element inside of adjacent
-            this._renderer.appendChild(
-                this._el.nativeElement,
-                this._svgComp.injector.get(InlineSVGComponent)._el.nativeElement
-            );
-        } else {
-            this._inlineSVGService.insertEl(this, this._el.nativeElement, el, this.replaceContents, this.prepend);
+    // Force evaluation of <style> tags since IE doesn't do it.
+    // See https://github.com/arkon/ng-inline-svg/issues/17
+    if (this.forceEvalStyles) {
+      const styleTags = svg.querySelectorAll('style');
+      Array.from(styleTags).forEach(tag => tag.textContent += '');
+    }
+
+    this.onSVGInserted.emit(svg);
+  }
+
+  private _svgElementFromString(str: string): SVGElement | never {
+    const div = this._renderer.createElement('DIV');
+    div.innerHTML = str;
+
+    return div.querySelector('svg') as SVGElement;
+  }
+
+  private _insertEl(el: Element): void {
+    if (this.injectComponent) {
+      if (!this._svgComp) {
+        const factory = this._resolver.resolveComponentFactory(InlineSVGComponent);
+        this._svgComp = this._viewContainerRef.createComponent(factory);
+      }
+
+      this._svgComp.instance.context = this;
+      this._svgComp.instance.replaceContents = this.replaceContents;
+      this._svgComp.instance.prepend = this.prepend;
+      this._svgComp.instance.content = el;
+
+      // Force element to be inside the directive element inside of adjacent
+      this._renderer.appendChild(
+          this._el.nativeElement,
+          this._svgComp.injector.get(InlineSVGComponent)._el.nativeElement
+      );
+    } else {
+      this._inlineSVGService.insertEl(this, this._el.nativeElement, el, this.replaceContents, this.prepend);
+    }
+  }
+
+  private _removeAttributes(svg: SVGElement, attrs: Array<string>): void {
+    if (!isPlatformBrowser(this.platformId)) { return; }
+
+    const innerEls = svg.getElementsByTagName('*');
+
+    for (let i = 0; i < innerEls.length; i++) {
+      const elAttrs = innerEls[i].attributes;
+
+      for (let j = 0; j < elAttrs.length; j++) {
+        if (attrs.indexOf(elAttrs[j].name.toLowerCase()) > -1) {
+          innerEls[i].removeAttribute(elAttrs[j].name);
         }
+      }
+    }
+  }
+
+  // Based off of code from https://github.com/iconic/SVGInjector
+  private _evalScripts(svg: SVGElement, url: string): void {
+    if (!isPlatformBrowser(this.platformId)) { return; }
+
+    const scripts = svg.querySelectorAll('script');
+    const scriptsToEval = [];
+    let script, scriptType;
+
+    // Fetch scripts from SVG
+    for (let i = 0; i < scripts.length; i++) {
+      scriptType = scripts[i].getAttribute('type');
+
+      if (!scriptType || scriptType === 'application/ecmascript' || scriptType === 'application/javascript') {
+        script = scripts[i].innerText || scripts[i].textContent;
+        scriptsToEval.push(script);
+        svg.removeChild(scripts[i]);
+      }
     }
 
-    private _removeAttributes(svg: SVGElement, attrs: Array<string>): void {
-        if (!isPlatformBrowser(this.platformId)) { return; }
+    // Run scripts in closure as needed
+    if (scriptsToEval.length > 0 && (this.evalScripts === 'always' ||
+        (this.evalScripts === 'once' && !this._ranScripts[url]))) {
+      for (let i = 0; i < scriptsToEval.length; i++) {
+        new Function(scriptsToEval[i])(window);
+      }
 
-        const innerEls = svg.getElementsByTagName('*');
-
-        for (let i = 0; i < innerEls.length; i++) {
-            const elAttrs = innerEls[i].attributes;
-
-            for (let j = 0; j < elAttrs.length; j++) {
-                if (attrs.indexOf(elAttrs[j].name.toLowerCase()) > -1) {
-                    innerEls[i].removeAttribute(elAttrs[j].name);
-                }
-            }
-        }
+      this._ranScripts[url] = true;
     }
+  }
 
-    // Based off of code from https://github.com/iconic/SVGInjector
-    private _evalScripts(svg: SVGElement, url: string): void {
-        if (!isPlatformBrowser(this.platformId)) { return; }
+  private _fail(msg: string): void {
+    this.onSVGFailed.emit(msg);
 
-        const scripts = svg.querySelectorAll('script');
-        const scriptsToEval = [];
-        let script, scriptType;
+    // Insert fallback image, if specified
+    if (this.fallbackImgUrl) {
+      const elImg = this._renderer.createElement('IMG');
+      this._renderer.setAttribute(elImg, 'src', this.fallbackImgUrl);
 
-        // Fetch scripts from SVG
-        for (let i = 0; i < scripts.length; i++) {
-            scriptType = scripts[i].getAttribute('type');
-
-            if (!scriptType || scriptType === 'application/ecmascript' || scriptType === 'application/javascript') {
-                script = scripts[i].innerText || scripts[i].textContent;
-                scriptsToEval.push(script);
-                svg.removeChild(scripts[i]);
-            }
-        }
-
-        // Run scripts in closure as needed
-        if (scriptsToEval.length > 0 && (this.evalScripts === 'always' ||
-            (this.evalScripts === 'once' && !this._ranScripts[url]))) {
-            for (let i = 0; i < scriptsToEval.length; i++) {
-                new Function(scriptsToEval[i])(window);
-            }
-
-            this._ranScripts[url] = true;
-        }
+      this._insertEl(elImg);
     }
+  }
 
-    private _fail(msg: string): void {
-        this.onSVGFailed.emit(msg);
-
-        // Insert fallback image, if specified
-        if (this.fallbackImgUrl) {
-            const elImg = this._renderer.createElement('IMG');
-            this._renderer.setAttribute(elImg, 'src', this.fallbackImgUrl);
-
-            this._insertEl(elImg);
-        }
-    }
-
-    private _checkSVGSupport(): boolean {
-        return typeof SVGRect !== 'undefined';
-    }
+  private _checkSVGSupport(): boolean {
+    return typeof SVGRect !== 'undefined';
+  }
 
 }
