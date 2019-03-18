44ba231091cc362b69fccee1b8c0670a0a943903
Anthony Mandra <anthony.mandra@keysight.com>
2019-03-18 13:52:28 -0400

    patch for ng4 compiler

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'demo',
   template: `
-    <div class="demo-svg1" aria-label="My icon 1" [inlineSVG]="'img/image.svg'" (onSVGLoaded)="handleSVG()" [removeSVGAttributes]="['xmlns']"></div>
-    <div class="demo-svg2" aria-label="My icon 2" [inlineSVG]="'img/image.svg'" [replaceContents]="true">Content</div>
-    <div *ngIf="_showOther" class="demo-svg3" aria-label="My delayed icon" [inlineSVG]="'img/image_with_fill.svg'" [removeSVGAttributes]="['fill']"></div>
-    <div [inlineSVG]="'img/symbol.svg#fish'"></div>
+    <div class="demo-svg1" aria-label="My icon 1" [inlineSVG]="'assets/image.svg'" (onSVGLoaded)="handleSVG()" [removeSVGAttributes]="['xmlns']"></div>
+    <div class="demo-svg2" aria-label="My icon 2" [inlineSVG]="'assets/image.svg'" [replaceContents]="true">Content</div>
+    <div *ngIf="_showOther" class="demo-svg3" aria-label="My delayed icon" [inlineSVG]="'assets/image_with_fill.svg'" [removeSVGAttributes]="['fill']"></div>
+    <div [inlineSVG]="'assets/symbol.svg#fish'"></div>
     <div [inlineSVG]="'#fish'"></div>
     <div [inlineSVG]="'#fish'" [injectComponent]="true"></div>
-    <div [inlineSVG]="'img/nope.svg'" [fallbackImgUrl]="'https://nodei.co/npm/ng-inline-svg.png?compact=true'"></div>
+    <div [inlineSVG]="'assets/nope.svg'" [fallbackImgUrl]="'https://nodei.co/npm/ng-inline-svg.png?compact=true'"></div>
   `
 })
 export class DemoComponent implements OnInit {
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DemoComponent } from './app.component';
 import { InlineSVGModule } from '../../../ng-inline-svg/src/lib/inline-svg.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { InlineSVGModule } from '../../../ng-inline-svg/src/lib/inline-svg.modul
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     InlineSVGModule.forRoot()
   ],
   providers: [],
